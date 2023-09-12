ff0e96231675aee77d50684edcd1acc00a042948
kyram <bruno.jimenez@laplateforme.io>
2023-09-12 11:47:16 +0200

    Delete Documents/projet/Runtrack/runtrack-b2-js/Jour-03 directory

--- Documents/projet/Runtrack/runtrack-b2-js/Jour-03/class/Player.js
+++
@@ -1,9 +0,0 @@
-class Player {
-    constructor(symbol, isCurrentPlayer) {
-        this.symbol = symbol;
-        this.isCurrentPlayer = isCurrentPlayer;
-    }
-}
-
-const playerX = new Player('X', true);
-const playerO = new Player('O', false);
